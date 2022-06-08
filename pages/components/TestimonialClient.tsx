76e57898413d419fa3f553189c2f052304d9836e
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-06-08 08:35:43 -0300

    feat: create component TestimonialClient

---
+++ pages/components/TestimonialClient.tsx
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import Slider from 'react-slick';
+
+type Testimonial = {
+    id: string;
+    name: string;
+    testimonial: string;
+    image: string;
+    date: string;
+};
+
+type Props = {
+    testimonials: Testimonial[];
+};
+
+const TestimonialClient = ({ testimonials }: Props) => {
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
+    return (
+        <Slider {...settings}>
+            {testimonials &&
+                testimonials.map((testimonial) => (
+                    <div key={testimonial.id} className='h-full text-center px-0 sm:px-4'>
+                        <Image
+                            width={80}
+                            height={80}
+                            alt='testimonial'
+                            className='w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100'
+                            src={testimonial.image}
+                        />
+                        <p className='leading-relaxed text-sm'>{testimonial.testimonial}</p>
+                        <span className='inline-block h-1 w-10 rounded bg-yellow-500 mt-6 mb-4'></span>
+                        <h2 className='text-gray-900 font-medium title-font tracking-wider text-sm'>
+                            {testimonial.name}
+                        </h2>
+                        <p className='text-gray-500 text-xs'>{testimonial.date}</p>
+                    </div>
+                ))}
+        </Slider>
+    );
+};
+
+export default TestimonialClient;
