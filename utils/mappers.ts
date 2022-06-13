f7fb3f2067e4046381306abe4c920b63c241a515
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-06-13 10:36:25 -0300

    feat: create function mappers

---
+++ utils/mappers.ts
@@ -0,0 +1,28 @@
+export const ProductsMapper = (products: any) => {
+    const mapper = products.results.map((data: any) => {
+        const dataProducts = {
+            id: data.id,
+            slug: data.uid,
+            image: data.data.image,
+            name: data.data.name,
+            description: data.data.description,
+            price: data.data.price,
+        };
+
+        if (data.data.available || data.data.available === false) {
+            return {
+                ...dataProducts,
+                available: data.data.available,
+            };
+        }
+
+        return {
+            ...dataProducts,
+            sizeP: data.data['size-p'] ? data.data['size-p'] : '',
+            sizeM: data.data['size-m'] ? data.data['size-m'] : '',
+            sizeG: data.data['size-g'] ? data.data['size-g'] : '',
+        };
+    });
+
+    return mapper;
+};
