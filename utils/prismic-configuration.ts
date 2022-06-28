d067e7ee676408cb68674263c2cdb88d908413b9
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-06-28 09:36:31 -0300

    fix: remove .env

--- utils/prismic-configuration.ts
+++ utils/prismic-configuration.ts
@@ -1,7 +1,8 @@
 import * as prismic from '@prismicio/client';
 
 // Prismic API endpoint
-export const apiEndpoint = String(process.env.NEXT_APP_API_URL);
+export const apiEndpoint = 'https://mari-modas.prismic.io/api/v2';
+
 // Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
 export const accessToken = '';
