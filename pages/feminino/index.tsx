85e858f63e6d08b4602c6afd7eb32419db3fc2bf
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-06-28 09:37:28 -0300

    feat: create Page Feminine

--- pages/feminino/index.tsx
+++ pages/feminino/index.tsx
@@ -1,9 +1,73 @@
 import Image from 'next/image';
 import Header from '../../components/Header';
-import BannerFeminino from '../../public/images/banner-feminino.jpg';
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import { client } from '../../utils/prismic-configuration';
+import * as prismic from '@prismicio/client';
+import { useEffect, useState } from 'react';
+import formatCurrent from '../../utils/formatCurrent';
+import { BiLastPage, BiFirstPage } from 'react-icons/bi';
 
-const Feminino = () => {
+type Products = {
+    id: string;
+    slug: string;
+    image: [{ image1: { url: string } }];
+    name: string;
+    description: string;
+    price: number;
+    pieceSize: [{ size: string; active: boolean }];
+    routeCategory: string;
+};
+
+type Props = {
+    products: Products[];
+    page: number;
+    totalPage: number;
+};
+
+const Feminine = ({ products: getProducts, page, totalPage }: Props) => {
+    const [products, setProducts] = useState(getProducts || []);
+    const [currentPage, setCurrentPage] = useState(Number(page));
+
+    console.log(currentPage);
+
+    const reqProducts = async (pageNumber: number) => {
+        const resultFeminine = await client.query(
+            prismic.Predicates.at('document.type', 'feminino'),
+            {
+                orderings: ['document.last_publication_date desc'],
+                pageSize: 1,
+                page: pageNumber,
+            }
+        );
+
+        console.log(resultFeminine);
+        return resultFeminine;
+    };
+
+    const navigatePage = async (pageNumber: number) => {
+        console.log(pageNumber);
+
+        const response = await reqProducts(pageNumber);
+
+        if (response.results.length === 0) return;
+
+        const products = response.results.map((product) => ({
+            id: product.id,
+            image: product.data.image,
+            name: product.data.name,
+            description: product.data.description,
+            price: product.data.price,
+        }));
+
+        setCurrentPage(pageNumber);
+        setProducts(products as Products[]);
+    };
+
+    console.log(page);
+
+    useEffect(() => {}, [page]);
+
     return (
         <>
             <Head>
@@ -30,136 +94,69 @@ const Feminino = () => {
                 </div>
 
                 <div className='grid gap-4 sm:gap-6 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 italic lg:grid-cols-5 my-6 mx-4 sm:mx-0'>
-                    <div className='mb-6'>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-2'>
-                            Vestido-Short com detalhes Vestido-Short com detalhes Vestido-Short com
-                            detalhes
-                        </h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div className='mb-6'>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='duration-300 transform hover:scale-110 hover:sepia hover:opacity-70 cursor-pointer'
-                            src={BannerFeminino}
-                            width={480}
-                            height={600}
-                            objectFit='cover'
-                            alt='Banner03'
-                        />
-                        <h4 className='text-gray-500 text-sm mt-4'>Vestido-Short com detalhes</h4>
-                        <p className='text-gray-800 font-bold'>R$ 45,50</p>
-                    </div>
+                    {products.map((product) => (
+                        <div className='mb-6' key={product.id}>
+                            <Image
+                                className='duration-300 transform hover:scale-110 hover:opacity-70 cursor-pointer'
+                                src={product.image[0].image1.url}
+                                width={480}
+                                height={600}
+                                objectFit='cover'
+                                alt={product.name}
+                            />
+                            <h4 className='text-gray-500 text-sm mt-2'>{product.name}</h4>
+                            <p className='text-gray-800 font-bold'>
+                                {formatCurrent(product.price)}
+                            </p>
+                        </div>
+                    ))}
                 </div>
 
                 <div className='flex justify-center my-20'>
-                    <div className='btn-group'>
-                        <button className='btn'>«</button>
-                        <button className='btn'>Page 22</button>
-                        <button className='btn'>»</button>
+                    <div className='flex justify-between sm:mx-0 mx-6 mb-10'>
+                        <div className='container flex justify-center mx-auto'>
+                            <ul className='flex'>
+                                <li>
+                                    <button
+                                        onClick={() => navigatePage(1)}
+                                        className={`${
+                                            currentPage === 1
+                                                ? 'hidden'
+                                                : 'font-semibold text-xs sm:text-base h-7 px-3 sm:h-8 sm:px-4 text-gray-600 bg-white hover:bg-gray-100'
+                                        }`}
+                                    >
+                                        <BiFirstPage size={20} />
+                                    </button>
+                                </li>
+                                {[...new Array(totalPage)].map((value, index) => (
+                                    <li>
+                                        <button
+                                            disabled={currentPage === index + 1 ? true : false}
+                                            onClick={() => navigatePage(index + 1)}
+                                            className={` ${
+                                                currentPage === index + 1
+                                                    ? ' text-gray-600 bg-yellow-500 shadow-lg cursor-not-allowed'
+                                                    : 'text-gray-600 bg-white hover:bg-gray-100'
+                                            } font-semibold text-xs sm:text-base h-7 px-3 sm:h-8 sm:px-4 `}
+                                        >
+                                            {index + 1}
+                                        </button>
+                                    </li>
+                                ))}
+                                <li>
+                                    <button
+                                        onClick={() => navigatePage(totalPage)}
+                                        className={`${
+                                            currentPage === totalPage
+                                                ? 'hidden'
+                                                : 'font-semibold text-xs sm:text-base h-7 px-3 sm:h-8 sm:px-4 text-gray-600 bg-white hover:bg-gray-100'
+                                        }`}
+                                    >
+                                        <BiLastPage size={20} />
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </Header>
@@ -167,4 +164,30 @@ const Feminino = () => {
     );
 };
 
-export default Feminino;
+export default Feminine;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    console.log(client);
+
+    const resultFeminine = await client.query(prismic.Predicates.at('document.type', 'feminino'), {
+        orderings: ['document.last_publication_date desc'],
+        pageSize: 1,
+        page: 1,
+    });
+
+    const products = resultFeminine.results.map((product) => ({
+        id: product.id,
+        image: product.data.image,
+        name: product.data.name,
+        description: product.data.description,
+        price: product.data.price,
+    }));
+
+    return {
+        props: {
+            products,
+            page: resultFeminine.page,
+            totalPage: resultFeminine.total_pages,
+        },
+    };
+};
