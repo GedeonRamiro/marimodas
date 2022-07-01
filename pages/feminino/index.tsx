0b46ce5bebc99ec24fa0ba4ef307f9010f894764
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-07-01 09:28:19 -0300

    feat: productsOrderAction

--- pages/feminino/index.tsx
+++ pages/feminino/index.tsx
@@ -7,6 +7,7 @@ import * as prismic from '@prismicio/client';
 import { useEffect, useState } from 'react';
 import formatCurrent from '../../utils/formatCurrent';
 import { BiLastPage, BiFirstPage } from 'react-icons/bi';
+import Link from 'next/link';
 
 type Products = {
     id: string;
@@ -27,27 +28,63 @@ type Props = {
 
 const Feminine = ({ products: getProducts, page, totalPage }: Props) => {
     const [products, setProducts] = useState(getProducts || []);
+    const [valueOrder, setValueOrder] = useState('');
     const [currentPage, setCurrentPage] = useState(Number(page));
 
-    console.log(currentPage);
+    const productsOrderAction = (order: string) => {
+        setValueOrder(order);
+
+        const newsProducts = [...products];
+
+        if (order === 'newsProducts') {
+            setProducts(getProducts);
+        }
+
+        if (order === 'alphabeticalOrder') {
+            const result = newsProducts.sort((x, y) => {
+                let a = x.name.toLocaleLowerCase(),
+                    b = y.name.toLocaleLowerCase();
+
+                return a == b ? 0 : a > b ? 1 : -1;
+            });
+
+            setProducts(result);
+        }
+
+        if (order === 'smallerPrice') {
+            console.log('Menor preco');
+            const result = newsProducts.sort((x, y) => {
+                return x.price - y.price;
+            });
+
+            setProducts(result);
+        }
+
+        if (order === 'largerPrice') {
+            console.log('Maior preco');
+
+            const result = newsProducts.sort((x, y) => {
+                return y.price - x.price;
+            });
+
+            setProducts(result);
+        }
+    };
 
     const reqProducts = async (pageNumber: number) => {
         const resultFeminine = await client.query(
             prismic.Predicates.at('document.type', 'feminino'),
             {
                 orderings: ['document.last_publication_date desc'],
-                pageSize: 1,
+                pageSize: 4,
                 page: pageNumber,
             }
         );
 
-        console.log(resultFeminine);
         return resultFeminine;
     };
 
     const navigatePage = async (pageNumber: number) => {
-        console.log(pageNumber);
-
         const response = await reqProducts(pageNumber);
 
         if (response.results.length === 0) return;
@@ -64,9 +101,7 @@ const Feminine = ({ products: getProducts, page, totalPage }: Props) => {
         setProducts(products as Products[]);
     };
 
-    console.log(page);
-
-    useEffect(() => {}, [page]);
+    useEffect(() => {}, [valueOrder]);
 
     return (
         <>
@@ -82,33 +117,39 @@ const Feminine = ({ products: getProducts, page, totalPage }: Props) => {
                 </blockquote>
 
                 <div className='flex justify-end items-center mx-4 sm:mx-0'>
-                    <select className='select select-sm  select-bordered  w-full max-w-[140px]'>
-                        <option disabled selected>
+                    <select
+                        defaultValue=''
+                        onChange={(event) => productsOrderAction(event.target.value)}
+                        className='select select-sm  select-bordered  w-full max-w-[140px]'
+                    >
+                        <option value='' disabled>
                             Ordenar por
                         </option>
-                        <option>Mais Novos</option>
-                        <option>Nome A-Z</option>
-                        <option>Menor Preço</option>
-                        <option>Maior Preço</option>
+                        <option value='newsProducts'>Mais Novos</option>
+                        <option value='alphabeticalOrder'>Nome A-Z</option>
+                        <option value='smallerPrice'>Menor Preço</option>
+                        <option value='largerPrice'>Maior Preço</option>
                     </select>
                 </div>
 
                 <div className='grid gap-4 sm:gap-6 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 italic lg:grid-cols-5 my-6 mx-4 sm:mx-0'>
                     {products.map((product) => (
-                        <div className='mb-6' key={product.id}>
-                            <Image
-                                className='duration-300 transform hover:scale-110 hover:opacity-70 cursor-pointer'
-                                src={product.image[0].image1.url}
-                                width={480}
-                                height={600}
-                                objectFit='cover'
-                                alt={product.name}
-                            />
-                            <h4 className='text-gray-500 text-sm mt-2'>{product.name}</h4>
-                            <p className='text-gray-800 font-bold'>
-                                {formatCurrent(product.price)}
-                            </p>
-                        </div>
+                        <Link href={`/feminino/${product.slug}`} key={product.id}>
+                            <div className='mb-6' key={product.id}>
+                                <Image
+                                    className='duration-300 transform hover:scale-110 hover:opacity-70 cursor-pointer'
+                                    src={product.image[0].image1.url}
+                                    width={480}
+                                    height={600}
+                                    objectFit='cover'
+                                    alt={product.name}
+                                />
+                                <h4 className='text-gray-500 text-sm mt-2'>{product.name}</h4>
+                                <p className='text-gray-800 font-bold'>
+                                    {formatCurrent(product.price)}
+                                </p>
+                            </div>
+                        </Link>
                     ))}
                 </div>
 
@@ -129,7 +170,7 @@ const Feminine = ({ products: getProducts, page, totalPage }: Props) => {
                                     </button>
                                 </li>
                                 {[...new Array(totalPage)].map((value, index) => (
-                                    <li>
+                                    <li key={index}>
                                         <button
                                             disabled={currentPage === index + 1 ? true : false}
                                             onClick={() => navigatePage(index + 1)}
@@ -167,16 +208,15 @@ const Feminine = ({ products: getProducts, page, totalPage }: Props) => {
 export default Feminine;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    console.log(client);
-
     const resultFeminine = await client.query(prismic.Predicates.at('document.type', 'feminino'), {
         orderings: ['document.last_publication_date desc'],
-        pageSize: 1,
+        pageSize: 4,
         page: 1,
     });
 
     const products = resultFeminine.results.map((product) => ({
         id: product.id,
+        slug: product.uid,
         image: product.data.image,
         name: product.data.name,
         description: product.data.description,
